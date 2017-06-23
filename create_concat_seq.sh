4ad7ecf9e9aa36c016756f2ce8f12a8204e412e0
Daniel Trone <royhobbstn@yahoo.com>
2017-06-23 12:19:45 +0000

    remove 2 lines of awk,  replace with 80 lines of nodejs

--- create_concat_seq.sh
+++ create_concat_seq.sh
@@ -25,19 +25,17 @@ echo -n -e ",\n" >> ./schemas/schema0002.txt; cat ./schemas/schema0002.txt eseq0
 
 echo -n -e ",\n" >> ./schemas/schema0003.txt; cat ./schemas/schema0003.txt eseq003.csv > ./readyfiles/eseq003.csv;
 
-cd readyfiles
 
-      
-awk -v FPAT="([^,]*)|(\"[^\"]+\")" -F $',' '{$1=$2=$3=$4=$5=$6=$7=$8=$9=$10=$11=$12=$13=$14=$15=$16=$17=$18=$19=$20=$21=$22=$23=$24=$25=$26=$27=$28=$29=$30=$31=$32=$33=$34=$35=$36=$37=$38=$39=$40=$41=$42=$43=$44=$45=$46=$47=$48=$49=$50=$51=$52=$53=$54=$55=""; print $0}' OFS=$',' eseq002.csv > seq2.txt
-
-awk -v FPAT="([^,]*)|(\"[^\"]+\")" -F $',' '{$1=$2=$3=$4=$5=$6=$7=$8=$9=$10=$11=$12=$13=$14=$15=$16=$17=$18=$19=$20=$21=$22=$23=$24=$25=$26=$27=$28=$29=$30=$31=$32=$33=$34=$35=$36=$37=$38=$39=$40=$41=$42=$43=$44=$45=$46=$47=$48=$49=$50=$51=$52=$53=$54=$55=""; print $0}' OFS=$',' eseq003.csv > seq3.txt
-
-cut -c 56- seq2.txt > seq2.ooo
-cut -c 56- seq3.txt > seq3.ooo
+head -1000 ./readyfiles/eseq001.csv > ./readyfiles/heseq001.csv
+head -1000 ./readyfiles/eseq002.csv > ./readyfiles/heseq002.csv
+head -1000 ./readyfiles/eseq003.csv > ./readyfiles/heseq003.csv
 
 
-# paste -d , eseq001.csv seq2.txt seq3.txt > eseqCAT001002003.csv
+git clone https://github.com/royhobbstn/tileCSVmerge.git
 
-paste -d , eseq001.csv seq2.ooo seq3.ooo > paste.csv
+cd tileCSVmergehead
+
+npm install
 
 
+# nodejs script

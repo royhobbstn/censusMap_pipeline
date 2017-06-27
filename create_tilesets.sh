7899f6389d912c83b0a7cf539ad35cfbecd446c1
Daniel Trone <royhobbstn@yahoo.com>
2017-06-27 11:43:34 +0000

    output basic selection of tilesets

--- create_tilesets.sh
+++ create_tilesets.sh
@@ -26,6 +26,18 @@ npm install -g mapshaper
 wget https://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_county_500k.zip
 wget https://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_state_500k.zip
 
+# repeat for all geo files COUNTRY
+for file in *.zip
+do unzip $file; done;
+# end repeat for all geo files
+
+
+# process shapefiles that are broken out into states
+
+mkdir stateshp
+
+cd stateshp
+
 ## declare an array variable
 declare -a arr=("01" "02" "04")
 
@@ -37,19 +49,17 @@ wget https://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_"$i"_place_500k.zip
 wget https://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_"$i"_tract_500k.zip
 done
 
-
-
-
-# repeat for all geo files
+# repeat for all geo files STATES
 for file in *.zip
 do unzip $file; done;
 # end repeat for all geo files
 
 # programmatically combine?  Separate routine?
-mapshaper -i *_tract_500k.shp combine-files \
-	-merge-layers \
-	-o pacific_states.shp
+mapshaper -i *_bg_500k.shp combine-files -merge-layers -o ../cb_2015_us_bg_500k.shp
+mapshaper -i *_place_500k.shp combine-files -merge-layers -o ../cb_2015_us_place_500k.shp
+mapshaper -i *_tract_500k.shp combine-files -merge-layers -o ../cb_2015_us_tract_500k.shp
 
+cd ..
 
 # repeat for all shp files
 for file in *.shp
@@ -60,11 +70,25 @@ do mapshaper $file -o format=geojson; done;
 # alter geoid to match between data and geo file 0400000US31
 sed -i -e 's/0400000US/04000US/g' cb_2015_us_county_500k.json
 sed -i -e 's/0500000US/05000US/g' cb_2015_us_state_500k.json
+sed -i -e 's/1500000US/15000US/g' cb_2015_us_bg_500k.json
+sed -i -e 's/1600000US/16000US/g' cb_2015_us_place_500k.json
+sed -i -e 's/1400000US/14000US/g' cb_2015_us_tract_500k.json
+
+
 # only retain geoid column
 tippecanoe -f -o acs1115_county.mbtiles -l county -z 10 -y GEOID -pk cb_2015_us_county_500k.json
 tippecanoe -f -o acs1115_state.mbtiles -l state -z 10 -y GEOID -pk cb_2015_us_state_500k.json
+tippecanoe -f -o acs1115_bg.mbtiles -l bg -z 10 -y GEOID -pk cb_2015_us_bg_500k.json
+tippecanoe -f -o acs1115_place.mbtiles -l place -z 10 -y GEOID -pk cb_2015_us_place_500k.json
+tippecanoe -f -o acs1115_tract.mbtiles -l tract -z 10 -y GEOID -pk cb_2015_us_tract_500k.json
 # end custom for each json file
 
+gsutil rm -r gs://mbtiles_staging
+gsutil mb gs://mbtiles_staging
+
+# copy all mbtiles files at once
+gsutil cp *.mbtiles gs://mbtiles_staging
+gsutil cp *.json gs://mbtiles_staging
 
 exit 1;
 
@@ -75,6 +99,7 @@ mkdir complete
 mkdir encoded
 mkdir outputmbtiles
 
+# TODO should be part of tileCSVmerge
 # for each CSV file
 # swap columns so geo key is first
 for $file in *.csv

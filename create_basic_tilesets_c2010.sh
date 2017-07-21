7d2e0e8a60ae61a5937d4419aa64dbfd984da014
Daniel Trone <royhobbstn@yahoo.com>
2017-07-21 11:54:41 +0000

    experiment with tippecanoe simplify

--- create_basic_tilesets_c2010.sh
+++ create_basic_tilesets_c2010.sh
@@ -41,7 +41,9 @@ mkdir stateshp
 cd stateshp
 
 ## declare an array variable
-declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "60" "66" "69" "72" "78")
+declare -a arr=("08" "09")
+
+# declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "60" "66" "69" "72" "78")
 
 ## now loop through the above array
 for i in "${arr[@]}"
@@ -87,7 +89,7 @@ do mapshaper $file -o format=geojson; done;
 # tippecanoe -f -o c2010_bg.mbtiles -l bg -z 10 -y GEO_ID -aL -D9 gz_2010_us_bg_500k.json
 # tippecanoe -f -o c2010_place.mbtiles -l place -z 10 -y GEO_ID -aL -D9 gz_2010_us_place_500k.json
 # tippecanoe -f -o c2010_tract.mbtiles -l tract -z 10 -y GEO_ID -aL -D9 gz_2010_us_tract_500k.json
-tippecanoe -f -o c2010_block.mbtiles -l block -z 10 -aL -D9 tl_2010_us_block.json
+tippecanoe -f -o c2010_block.mbtiles -l block -z 10 tl_2010_us_block.json
 # TODO blocks
 # end custom for each json file
 

ce226d28fb828ab95772cb432e08cfacb0e87344
Daniel Trone <royhobbstn@yahoo.com>
2017-07-31 22:50:32 +0000

    ready for big time

--- create_basic_block_tileset_c2010.sh
+++ create_basic_block_tileset_c2010.sh
@@ -29,10 +29,10 @@ cd stateshp
 
 mkdir block
 
-declare -a arr=("01" "02")
+# declare -a arr=("01" "02")
 
 # declare an array variable
-# declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "60" "66" "69" "72" "78")
+declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "60" "66" "69" "72" "78")
 
 ## now loop through the above array
 for i in "${arr[@]}"
@@ -73,7 +73,7 @@ tippecanoe -f -o c2010_block.mbtiles -l block -z 10 -y GEO_ID -pk -pf tl_2010_us
 # --drop-densest-as-needed
 # --no-tile-size-limit
 
-gsutil rm c2010_block.mbtiles gs://c2010_tiles_staging
+gsutil rm gs://c2010_tiles_staging/c2010_block.mbtiles
 
 # copy all mbtiles files at once
 gsutil cp *.mbtiles gs://c2010_tiles_staging

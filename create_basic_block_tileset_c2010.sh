95028ba325359e949b66cfc531c2aaf9ea581634
Daniel Trone <royhobbstn@yahoo.com>
2017-08-02 11:27:14 +0000

    incorporate node script

--- create_basic_block_tileset_c2010.sh
+++ create_basic_block_tileset_c2010.sh
@@ -29,10 +29,10 @@ cd stateshp
 
 mkdir block
 
-# declare -a arr=("01" "02")
+declare -a arr=("01" "02")
 
 # declare an array variable
-declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "60" "66" "69" "72" "78")
+# declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "60" "66" "69" "72" "78")
 
 ## now loop through the above array
 for i in "${arr[@]}"
@@ -48,24 +48,21 @@ for file in *.zip
 do unzip $file; done;
 # end repeat for all geo files
 
-# repeat for all shp files
-for file in *.shp
-do node --max_old_space_size=45056 ~/.nvm/versions/node/v8.2.1/bin/mapshaper $file -o format=geojson; done;
-# end repeat for all shp files 
-
-node --max_old_space_size=45056 ~/.nvm/versions/node/v8.2.1/bin/mapshaper -i tl_2010_*.json combine-files -merge-layers -o ../../tl_2010_us_block.json
-
-cd ..
-
-cd ..
+# convert all shp files
+mapshaper *.shp -o format=geojson;
 
 # custom per each json file
 # alter geoid to match between data and geo file
-sed -i -e 's/GEOID10/GEO_ID/g' tl_2010_us_block.json
+for file in *.json
+do echo "update key $file"; sed -i -e 's/GEOID10/GEO_ID/g' $file;
+done;
+
+npm install
+node merge_geojson_stream.js
 
 
 # only retain geoid column
-tippecanoe -f -o c2010_block.mbtiles -l block -z 10 -y GEO_ID -pk -pf tl_2010_us_block.json
+tippecanoe -f -o c2010_block.mbtiles -l block -z 10 -y GEO_ID -pk -pf output.geojson
 # end custom for each json file
 
 # try some of these:

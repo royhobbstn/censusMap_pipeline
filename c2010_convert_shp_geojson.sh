dc7eca1675d0d2c507f28444688661ed13110f9a
Daniel Trone <royhobbstn@yahoo.com>
2017-08-04 10:34:15 +0000

    break apart script

---
+++ c2010_convert_shp_geojson.sh
@@ -0,0 +1,63 @@
+
+rm -rf run
+
+mkdir run
+
+cd run
+
+sudo apt-get install -y unzip
+
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install node 8.2.1
+
+npm install -g mapshaper
+
+
+mkdir stateshp
+
+cd stateshp
+
+mkdir block
+
+declare -a arr=("01" "02")
+
+# declare an array variable
+# declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "60" "66" "69" "72" "78")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+# tiger
+wget https://www2.census.gov/geo/tiger/TIGER2010/TABBLOCK/2010/tl_2010_"$i"_tabblock10.zip -P block/
+done
+
+cd block
+
+# repeat for all geo files STATES
+for file in *.zip
+do unzip $file; done;
+# end repeat for all geo files
+
+# convert all shp files
+mapshaper *.shp -o format=geojson;
+
+# custom per each json file
+# alter geoid to match between data and geo file
+for file in *.json
+do echo "update key $file"; sed -i -e 's/GEOID10/GEO_ID/g' $file;
+done;
+
+for file in *.json
+do
+ mv "$file" "${file%.json}.geojson"
+done
+
+gsutil rb -f gs://c2010_block_tiles_staging
+
+gsutil mb gs://c2010_block_tiles_staging
+
+# copy all mbtiles files at once
+gsutil cp *.geojson gs://c2010_block_tiles_staging
--- create_basic_block_tileset_c2010.sh
+++ create_basic_block_tileset_c2010.sh
@@ -1,4 +1,5 @@
 
+
 rm -rf run
 
 mkdir run
@@ -7,63 +8,18 @@ cd run
 
 sudo apt-get install -y unzip
 
-sudo apt-get install -y ruby
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)" < /dev/null
-PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bash_profile
-
-brew install tippecanoe
-
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install node 8.2.1
 
-npm install -g mapshaper
+sudo apt-get install -y ruby
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)" < /dev/null
+PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bash_profile
 
-
-mkdir stateshp
-
-cd stateshp
-
-mkdir block
-
-declare -a arr=("01" "02")
-
-# declare an array variable
-# declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "60" "66" "69" "72" "78")
-
-## now loop through the above array
-for i in "${arr[@]}"
-do
-# tiger
-wget https://www2.census.gov/geo/tiger/TIGER2010/TABBLOCK/2010/tl_2010_"$i"_tabblock10.zip -P block/
-done
-
-cd block
-
-# repeat for all geo files STATES
-for file in *.zip
-do unzip $file; done;
-# end repeat for all geo files
-
-# convert all shp files
-mapshaper *.shp -o format=geojson;
-
-# custom per each json file
-# alter geoid to match between data and geo file
-for file in *.json
-do echo "update key $file"; sed -i -e 's/GEOID10/GEO_ID/g' $file;
-done;
-
-# TODO - bucketize individual state geojson
-
-# TODO - do attribute join here
-
-mv *.json ./../../../
-
-cd ../../../
+brew install tippecanoe
 
 npm install
 # node createTableCSV_c2010.js
@@ -90,30 +46,37 @@ cd divide
 
 # divide by state
 for file in *.txt; do echo "splitting $file into states
-"; awk -F ',' -v fn=`basename ${file%????}` '{print > fn$6".csv"}' $file; done;
+"; awk -F ',' -v fn=`basename ${file%????}` '{print > fn"_"$6".csv"}' $file; done;
 
 exit 1;
 
+# download geojson
 
-# join 
+# join with geojson
 
+# use https://github.com/node-geojson/geojson-join
+
+# merge all joined files together again
+
+# loop through
 node merge_geojson_stream.js
 
-
-# only retain geoid column
+# loop through
+# convert to tileset; only retain geoid column
 tippecanoe -f -o c2010_block.mbtiles -l block -z 10 -y GEO_ID -pk -pf output.geojson
 # end custom for each json file
+
+
 
 # try some of these:
 # --drop-smallest-as-needed
 # --drop-densest-as-needed
 # --no-tile-size-limit
 
-gsutil mb gs://c2010_block_tiles_staging
+gsutil rb -f gs://c2010_block_tiles
 
-gsutil rm gs://c2010_block_tiles_staging/c2010_block.mbtiles
+gsutil mb gs://c2010_block_tiles
 
 # copy all mbtiles files at once
-gsutil cp *.mbtiles gs://c2010_block_tiles_staging
-# gsutil cp *.json gs://c2010_tiles_staging
+gsutil cp *.mbtiles gs://c2010_block_tiles
 

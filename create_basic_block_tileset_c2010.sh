34a589fc0d8c9928b39f3c007da29ee05eea5813
Daniel Trone <royhobbstn@yahoo.com>
2017-08-06 11:47:50 +0000

    it works!

--- create_basic_block_tileset_c2010.sh
+++ create_basic_block_tileset_c2010.sh
@@ -59,7 +59,7 @@ mkdir header
 
 
 # declare an array variable
-declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "60" "66" "69" "72" "78")
+declare -a arr=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56" "72")
 
 for j in "${arr[@]}"; do
 echo "adding header to state: $j"
@@ -97,9 +97,13 @@ mkdir tiles
 # merge all joined files together again
 node merge_geojson_stream.js
 
+cd tiles
+
 # loop through
 # convert to tileset; only retain geoid column
-tippecanoe -f -o c2010_block.mbtiles -l block -z 10 -y GEO_ID -pk -pf output.geojson
+for k in *.geojson; do
+tippecanoe -f -o ${k%????????}_c2010_block.mbtiles -l block -z 10 -pk -pf $k
+done;
 # end custom for each json file
 
 
